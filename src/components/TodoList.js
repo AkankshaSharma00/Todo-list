b6727bb0c3d3464b49b230b6b359f227a28d2831
AkankshaSharma00 <sharmaakanksha1205@gmail.com>
2019-05-23 05:01:19 +0530

    Styling todo-list

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,23 +5,23 @@ class TodoList extends Component{
     render(){
         return(
             <Container>
-            <Form onSubmit={this.props.addItem}>
-                <center>
-                <Form.Field>
-                    <input 
-                        type="text" 
-                        placeholder ="Add Task" 
-                        ref={this.props.inputElement}
-                        value={this.props.currentItem.text}
-                        onChange={this.props.handleInput}
-                    />
-                </Form.Field>
-                <Button type="submit" animated color='blue'>
-                    <Button.Content visible>Add Task</Button.Content>
-                    <Button.Content hidden><Icon name="pencil alternate" /></Button.Content>
-                </Button>
-                </center>
-            </Form>
+                <Form onSubmit={this.props.addItem}>
+                    <center>
+                    <Form.Field>
+                        <input 
+                            type="text" 
+                            placeholder ="Add Task" 
+                            ref={this.props.inputElement}
+                            value={this.props.currentItem.text}
+                            onChange={this.props.handleInput}
+                        />
+                    </Form.Field>
+                    <Button type="submit" animated color='blue'>
+                        <Button.Content visible>Add Task</Button.Content>
+                        <Button.Content hidden><Icon name="pencil alternate" /></Button.Content>
+                    </Button>
+                    </center>
+                </Form>
             </Container>
         );
     }
